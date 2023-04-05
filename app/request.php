1ce80d1f0b965393f86d48328996c3d927f7cbaa
Nikos Koutelias <nkoutelias@sqlearn.com>
2023-04-05 18:08:14 +0300

    Fixes

--- app/request.php
+++ app/request.php
@@ -12,10 +12,25 @@ use League\CommonMark\CommonMarkConverter;
 $openai = new OpenAi( $settings['api_key'] );
 $context = json_decode( $_POST['context'] ?? "[]" ) ?: [];
 
-//$open_ai = new OpenAi('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');
-// get prompt parameter
-// $prompt = "You are a maritime training consultant and you will only answer maritime related questions \n\n";
-// $prompt .= $_POST['prompt'];
+// Set up a mapping of file names to file paths for the PDF files
+// that you want to use for training your chatbot
+$pdf_files = array(
+    'RISQ.pdf' => 'docs/RISQ.pdf',
+    'file2.pdf' => 'path/to/file2.pdf',
+);
+
+// Loop through the PDF files, extract text, and concatenate the results
+$training_data = '';
+foreach ($pdf_files as $file_name => $file_path) {
+    $pdf_text = extract_text_from_pdf($file_path);
+    $training_data .= $pdf_text . "\n";
+}
+
+$c_file = curl_file_create(__DIR__ . 'files/sample_file_1.jsonl');
+$result = $open_ai->uploadFile([
+    "purpose" => "answers",
+    "file" => $c_file,
+]);
 
 $messages = [];
 
@@ -49,7 +64,7 @@ $complete = json_decode( $openai->chat( [
     'temperature' => 1.0,
     'max_tokens' => 2000,
     'frequency_penalty' => 0,
-    'presence_penalty' => 0,
+    'presence_penalty' => 0
     ] ) );
 
 // get message text
@@ -72,49 +87,14 @@ echo json_encode( [
     "status" => "success",
 ] );
 
-// Set up a mapping of file names to file paths for the PDF files
-// that you want to use for training your chatbot
-$pdf_files = array(
-    'RISQ.pdf' => 'docs/RISQ.pdf',
-    'file2.pdf' => 'path/to/file2.pdf',
-);
 
-// Loop through the PDF files, extract text, and concatenate the results
-$training_data = '';
-foreach ($pdf_files as $file_name => $file_path) {
-    $pdf_text = extract_text_from_pdf($file_path);
-    $training_data .= $pdf_text . "\n";
-}
-
-
-// OLD WAY
-// set api data
-// $complete = $open_ai->completion([
-//     'model' => 'text-davinci-003',
-//     'prompt' => $prompt,
-//     'temperature' => 0.2,
-//     'max_tokens' => 256,
-//     'frequency_penalty' => 0,
-//     'presence_penalty' => 0,
-//     'n' => 1,
-//     'stream' => true
-// ], function($curl_info, $data){
-//     // now we will get stream data
-//     echo $data;
-//     echo PHP_EOL;
-//     @ob_flush();
-//     flush();
-//     return strlen($data);
-// });
-
-
-//  echo ($prompt = $d->choices[0]->message->content);
 // Define a function to extract text from a PDF file
 function extract_text_from_pdf($pdf_path) {
     // Use the pdf2text library to extract text from the PDF file
     $pdf = new Pdf2text();
     return $pdf->decode($pdf_path);
 }
+
 
 
 ?>
--- app/settings.php
+++ app/settings.php
@@ -3,7 +3,7 @@
 
 return [
     // add your OpenAI API key here
-    "api_key" => "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",
+    "api_key" => "<token>",
 
     // add an optional system message here
     "system_message" => "You are a maritime training consultant and you will only answer maritime related questions \n\n",
